c3d8b591721bb1f823aaa198fe6bb6248a8e5eed
Bruno Lemes Dos Santos <bruno.santos@climatempo.com.br>
2025-11-15 20:21:13 -0300

    feat: add config jpa

--- src/main/java/com/engate/fitness/api/Application.java
+++ src/main/java/com/engate/fitness/api/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		IO.println("Starting Fitness API Application...");
 		SpringApplication.run(Application.class, args);
 	}
 
